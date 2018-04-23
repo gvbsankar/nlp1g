58423214873ec13e2d6c2e6ccead13cecb25776f
Bhavani Goda <bhavani.goda@intel.com>
2018-04-23 17:29:06 +0200

    dictionary files & script to load content

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Load English dictionary
+"""
+
+with open('.\dictionaryFiles\OEngDictionary.txt', encoding="utf8") as f:
+    lines = f.readlines()
+    print(lines[1])
